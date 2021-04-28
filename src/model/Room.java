2e39265f3e0381451fd2dbf4ef600167ae167700
Louis-VR-etu <etu44348@henallux.be>
2021-04-28 15:56:47 +0200

    room get set & constr

--- src/model/Room.java
+++ src/model/Room.java
@@ -2,4 +2,49 @@ package model;
 
 public class Room {
 
+    private Integer number;
+    private String hotelName;
+    private Integer floor;
+    private String roomType;
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setFloor(Integer floor) {
+        this.floor = floor;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+
+
+    Room(Integer number, Integer floor, String hotelName, String roomType){
+        this.number=number;
+        this.floor=floor;
+        this.roomType=roomType;
+        this.hotelName=hotelName;
+    }
 }
