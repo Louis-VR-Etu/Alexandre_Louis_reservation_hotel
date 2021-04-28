61b4fa33bb363f83c0c206fafc12682fc71136a9
Louis-VR-etu <etu44348@henallux.be>
2021-04-28 15:40:05 +0200

    roomType setter

--- src/RoomType.java
+++ src/RoomType.java
@@ -1,2 +1,20 @@
 public class RoomType {
+    private String typeName;
+    private float price;
+    private int singleBed;
+    private int doubleBed;
+
+    void setTypeName(String typeName){
+        this.typeName=typeName;
+    }
+    void setPrice(float price){
+        this.price = price;
+    }
+    void setSingleBed(int singleBed){
+        this.singleBed = singleBed;
+    }
+    void setDoubleBed(int doubleBed){
+        this.doubleBed=doubleBed;
+    }
+
 }
