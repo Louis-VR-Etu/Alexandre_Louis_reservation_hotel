013ed6beb4b90a87853146ffaa6acfabfc493726
Louis-VR-etu <etu44348@henallux.be>
2021-08-05 12:38:54 +0200

    ReservationManager

--- src/business/ReservationManager.java
+++ src/business/ReservationManager.java
@@ -20,34 +20,53 @@ public class ReservationManager {
 
     public void addReservations(Reservation reservation) throws AddReservationException {
         //todo
+        try {reservationDB.addReservation(reservation);}
+        catch(AddReservationException exception){ throw exception;}
     }
 
     public void deleteReservation(Reservation reservation)throws DeleteReservationException {
-        //todo
+        try{ reservationDB.deleteReservation(reservation);}
+        catch (DeleteReservationException exception){ throw exception;}
     }
 
     public void updateReservation(Reservation reservation, Reservation reservationUpdated)throws UpdateReservationException {
-        //todo
+        try{ reservationDB.updateReservation(reservation, reservationUpdated);}
+        catch (GetReservationException exception){ throw exception;}
     }
 
     public ArrayList<String> stringReservations(ArrayList<Reservation> reservations) {
         //todo
-        return null;
+        ArrayList<String> reservationString = new ArrayList<>();
+        for(int iReserv = 0 ; iReserv < reservations.size(); iReserv++){
+            //todo verifier String
+            reservationString.add(reservations.get(iReserv).getCustomerMail() + ", Hotel"+reservations.get(iReserv).getHotelName()+ "chambre "+reservations.get(iReserv).getRoomNumber()+ " du "+reservations.get(iReserv).getBeginningDate()+ " au "+reservations.get(iReserv).getEndingDate());
+        }
+
+        return  reservationString;
     }
 
     public Date verifyBeginningDate(String day, String month, String year) throws AddReservationException {
-        //todo
-        return null;
+        try {
+            //todo date
+            return new Date(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        }
+        catch (NumberFormatException exception) {
+            throw new AddReservationException("wrong beginning date");
+        }
     }
 
     public Integer verifyRoomNumber(String roomNumber) throws AddReservationException{
-        //todo
-        return null;
+        try {
+            return  verifyNumber(roomNumber);
+        }
+        catch (Exception exception) {
+            throw new AddReservationException("Wrong room number");
+        }
     }
 
     public String verifyHotelName(String hotelName)throws AddReservationException {
-        //todo
-        return null;
+        try{ return verifyString(hotelName);}
+        catch (Exception exception) {throw new AddReservationException("wrong hotel name");}
     }
 
     public Date verifyEndingDate(String day, String month, String year)throws AddReservationException {
@@ -85,3 +104,31 @@ public class ReservationManager {
         return null;
     }
 }
+
+
+//todo verifier conditions
+    public Integer verifyNumber(String number) throws Exception{
+        if (number.compareTo("") == 0) {
+            throw new Exception("number");
+        } else {
+            boolean isGood = true;
+            int iNumber = 0;
+            while (iNumber < number.length() && isGood) {
+                isGood = number.charAt(iNumber) == '0' || number.charAt(iNumber) == '1' || number.charAt(iNumber) == '2' || number.charAt(iNumber) == '3' || number.charAt(iNumber) == '4' || number.charAt(iNumber) == '5' || number.charAt(iNumber) == '6' || number.charAt(iNumber) == '7' || number.charAt(iNumber) == '8' || number.charAt(iNumber) == '9';
+                iNumber++;
+            }
+            if (isGood) {
+                return Integer.parseInt(number);
+            } else {
+                throw new Exception("number");
+            }
+        }
+    }
+
+    public String verifyString(String text) throws Exception {
+        if (text.compareTo("") == 0) {
+            throw new Exception("string");
+        } else {
+            return text;
+        }
+    }
