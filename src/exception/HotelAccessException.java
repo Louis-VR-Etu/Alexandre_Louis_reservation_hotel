869158b1ab11cde48130d804bcdb4c9799433747
Louis-VR-etu <etu44348@henallux.be>
2021-08-05 11:11:46 +0200

    add n

--- src/exception/HotelAccessException.java
+++ src/exception/HotelAccessException.java
@@ -1,5 +1,12 @@
 package exception;
 
 public class HotelAccessException extends Exception {
+    private String exception;
+    public HotelAccessException(String exception) {
+        this.exception = exception;
+    }
 
+    public String getMessage() {
+        return "HotelAccessException " + exception;
+    }
 }
