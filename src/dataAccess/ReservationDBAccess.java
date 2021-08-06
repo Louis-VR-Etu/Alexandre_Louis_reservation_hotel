0fb14344cf07bf6f50af5ad25bc04f35a22b4d0c
Louis-VR-etu <etu44348@henallux.be>
2021-08-06 10:59:21 +0200

    addReservation by DB

--- src/dataAccess/ReservationDBAccess.java
+++ src/dataAccess/ReservationDBAccess.java
@@ -7,7 +7,10 @@ import exception.UpdateReservationException;
 import model.Reservation;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 
 public class ReservationDBAccess {
@@ -37,7 +40,38 @@ public class ReservationDBAccess {
     }
 
     public void addReservation(Reservation reservation) throws AddReservationException {
-        //TODO
+        //TODO verifier avec DB
+
+//*
+        try {
+            Connection connection = SingletonConnexion.getInstance();
+            String sqlInstruction = "insert into reservation(beginningDate, roomNumber, roomHotelName, endingDate, allInclusive, people, remarks, additionalContact, couponCode, customerMail) values(str_to_date(?,'%d/%m/%Y'),?,?,str_to_date(?,'%d/%m/%Y'),?,?,?,?,?,?)";
+            connection.setAutoCommit(true);
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlInstruction);
+            GregorianCalendar date = new GregorianCalendar(reservation.getBeginningDate().YEAR, reservation.getBeginningDate().MONTH, reservation.getBeginningDate().DAY_OF_MONTH);
+            String pattern = "dd/MM/yyyy";
+            DateFormat df = new SimpleDateFormat(pattern);
+            String beginDate = df.format(date);
+            preparedStatement.setString(1,beginDate);
+            preparedStatement.setInt(2, reservation.getRoomNumber());
+            preparedStatement.setString(3, reservation.getHotelName());
+            GregorianCalendar date2 = new GregorianCalendar(reservation.getBeginningDate().YEAR, reservation.getBeginningDate().MONTH, reservation.getBeginningDate().DAY_OF_MONTH);
+            String endDate = df.format(date2);
+            preparedStatement.setString(4, endDate);
+            preparedStatement.setBoolean(5, reservation.getAllInclusive());
+
+            preparedStatement.setInt(6, reservation.getPeople());
+            preparedStatement.setString(8, reservation.getTitle());
+            preparedStatement.setString(9, reservation.getAdditionalContact());
+            preparedStatement.setString(9, reservation.getCouponCode());
+            preparedStatement.setString(10, reservation.getCustomerMail());
+            preparedStatement.executeUpdate();
+        }
+
+        catch (SQLException exception) {
+            throw new AddReservationException(exception.getMessage());
+        }
+        //*/
     }
 
     public void deleteReservation(Reservation reservation) throws DeleteReservationException {
