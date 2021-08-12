9a6400bd372b0516c775a535bce7236b5ab23172
Louis-VR-etu <etu44348@henallux.be>
2021-08-12 12:14:15 +0200

    UpdatePanel3

--- src/business/ReservationManager.java
+++ src/business/ReservationManager.java
@@ -186,7 +186,7 @@ public class ReservationManager {
 
     public String verifyString(String text) throws Exception {
         if (text.compareTo("") == 0) {
-            throw new Exception("string vide");
+            throw new Exception("Empty string");
         } else {
             return text;
         }
