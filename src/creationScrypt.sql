842800823e53e581980740255c5710a13e02e086
Louis-VR-etu <etu44348@henallux.be>
2021-08-12 15:15:28 +0200

    SQL remplissageScrypt

--- src/creationScrypt.sql
+++ src/creationScrypt.sql
@@ -14,8 +14,32 @@ CREATE TABLE `hotel` (
                          `animals` bit(1) NOT NULL,
                          `allInclusivePrice` float NOT NULL,
                          PRIMARY KEY (`name`),
-                         UNIQUE KEY `name_UNIQUE` (`name`)
-) ;
+                         UNIQUE KEY `name_UNIQUE` (`name`),
+                         CONSTRAINT `positive_price` check(`allInclusivePrice`>=0)
+);
+CREATE TABLE `roomtype` (
+                            `typeName` varchar(255) NOT NULL,
+                            `price/night` float NOT NULL ,
+                            `singleBed` int NOT NULL,
+                            `doubleBed` int NOT NULL ,
+                            PRIMARY KEY (`typeName`),
+                            UNIQUE KEY `typeName_UNIQUE` (`typeName`),
+                            CONSTRAINT `sgb_amount_pos` check(`singleBed`>=0),
+                            CONSTRAINT `sdb_amount_pos` check(`doubleBed`>=0),
+                            CONSTRAINT `pn_pos` check(`price/night`>0)
+);
+CREATE TABLE `room` (
+                        `number` int NOT NULL,
+                        `hotelName` varchar(255) NOT NULL,
+                        `floor` int NOT NULL,
+                        `roomTypeName` varchar(255) NOT NULL,
+                        PRIMARY KEY (`number`,`hotelName`),
+                        KEY `name_idx` (`hotelName`),
+                        KEY `roomTypename_idx` (`roomTypeName`),
+                        CONSTRAINT `hotelName` FOREIGN KEY (`hotelName`) REFERENCES `hotel` (`name`),
+                        CONSTRAINT `roomTypename` FOREIGN KEY (`roomTypeName`) REFERENCES `roomtype` (`typeName`),
+                        CONSTRAINT `number_pos` check(number>0)
+);
 CREATE TABLE `reservation` (
                                `beginningDate` date NOT NULL,
                                `roomNumber` int NOT NULL,
@@ -31,24 +55,8 @@ CREATE TABLE `reservation` (
                                KEY `roomNumber_idx` (`roomNumber`,`roomHotelName`),
                                KEY `customerReservation_idx` (`customerMail`),
                                CONSTRAINT `customerReservation` FOREIGN KEY (`customerMail`) REFERENCES `customer` (`mail`),
-                               CONSTRAINT `reservedRoom` FOREIGN KEY (`roomNumber`, `roomHotelName`) REFERENCES `room` (`number`, `hotelName`)
-);
-CREATE TABLE `room` (
-                        `number` int NOT NULL,
-                        `hotelName` varchar(255) NOT NULL,
-                        `floor` int NOT NULL,
-                        `roomTypeName` varchar(255) NOT NULL,
-                        PRIMARY KEY (`number`,`hotelName`),
-                        KEY `name_idx` (`hotelName`),
-                        KEY `roomTypename_idx` (`roomTypeName`),
-                        CONSTRAINT `hotelName` FOREIGN KEY (`hotelName`) REFERENCES `hotel` (`name`),
-                        CONSTRAINT `roomTypename` FOREIGN KEY (`roomTypeName`) REFERENCES `roomtype` (`typeName`)
-);
-CREATE TABLE `roomtype` (
-                            `typeName` varchar(255) NOT NULL,
-                            `price/night` float NOT NULL,
-                            `singleBed` int NOT NULL,
-                            `doubleBed` int NOT NULL,
-                            PRIMARY KEY (`typeName`),
-                            UNIQUE KEY `typeName_UNIQUE` (`typeName`)
+                               CONSTRAINT `reservedRoom` FOREIGN KEY (`roomNumber`, `roomHotelName`) REFERENCES `room` (`number`, `hotelName`),
+                               CONSTRAINT `begin_before_end` check (`beginningDate` < `endingDate`),
+                               CONSTRAINT `people_positive` check(`people`>0)
+
 );
